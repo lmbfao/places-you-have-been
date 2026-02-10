087f04df45b79ac51762e0f1f7d91cc7ede26725
Christine Karuga <christinewangechik@gmail.com>
2026-02-10 21:50:22 +0300

    define constructors properly

--- script.js
+++ script.js
@@ -26,21 +26,36 @@ Trip.prototype.addPlace = function(place) {
 
 const myTrip = new Trip();
 
-myTrip.addPlace(new Place("Dubai", "Burj Khalifa", "December 2016", " Can't wait to see the tallest building in the world!"));
-myTrip.addPlace(new Place("Florida", "Disney World", "April 2017", "Looking forward to the rides!"));
-myTrip.addPlace(new Place("Johannesburg", "Red bus tour", "November 2024", "Looking forward to exploring the city!"));
-myTrip.addPlace(new Place("Kuala Lumpur", "Petronas Towers", "April 2025", "Can't wait to see the tallest twin towers in the world!"));
+myTrip.addPlace(new Place("Dubai",
+  ["Burj Khalifa"],
+  "December 2016",
+  " Can't wait to see the tallest building in the world!"));
+
+myTrip.addPlace(new Place("Florida",
+  ["Disney World"],
+  "April 2017",
+  "Looking forward to the rides!"));
+
+myTrip.addPlace(new Place("Johannesburg",
+  ["Red bus tour"],
+  "November 2024",
+  "Looking forward to exploring the city!"));
+
+myTrip.addPlace(new Place("Kuala Lumpur",
+  ["Petronas Towers"],
+  "April 2025",
+  "Can't wait to see the tallest twin towers in the world!"));
 
 const placesList = document.getElementById("places-list");
+
+const detailsSection = document.getElementById("details-text");
+
 myTrip.places.forEach(place => {
   const listItem = document.createElement("li");
-  listItem.textContent = place.info();
+  listItem.textContent = place.location;
+  
+  listItem.addEventListener("click", function() {
+    detailsSection.textContent = place.info();
+  });
   placesList.appendChild(listItem);
-});
-
-const detailsSection = document.getElementById("details");
-placesList.addEventListener("click", function(event) {
-  if (event.target.tagName === "li") {
-    detailsSection.innerHTML = event.target.textContent;
-  }
 });
